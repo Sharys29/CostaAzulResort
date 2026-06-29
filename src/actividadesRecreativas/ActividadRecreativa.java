e4ea9ad5181dd3dcf5f1cacff6e41a6f0a95f132
sharys2906 <sharysceciliano@gmail.com>
2026-06-29 15:11:04 -0600

    Clase ActividadRecreativa
    se hace la clase actividad recreativa

--- src/actividadesRecreativas/ActividadRecreativa.java
+++ src/actividadesRecreativas/ActividadRecreativa.java
@@ -4,10 +4,54 @@
  */
 package actividadesRecreativas;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  *
  * @author Student
  */
 public class ActividadRecreativa {
+    
+    protected String nombre;
+    protected LocalDate fecha;
+    protected LocalTime hora;
+    protected String cupomax;
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public String getCupomax() {
+        return cupomax;
+    }
+
+    public ActividadRecreativa(String nombre, LocalDate fecha, LocalTime hora, String cupomax) {
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.cupomax = cupomax;
+    }
+    
+    
+    
+    
     
 }
